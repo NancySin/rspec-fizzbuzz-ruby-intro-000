5328eaf3727a9c78a690077a3bf1d245b14d1afd
nancy sin <nsin@sbcglobal.net>
2018-02-20 20:46:23 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
-def fizzbuzz
+def fizzbuzz(int)
 end
 
 def fizzbuzz(int)
   if int % 3 == 0 #if the number int is divisible by 3
     "Fizz" #
   elsif int % 5 == 0
-    "buzz"
+    "Buzz"
   elsif int % 3 == 0 && int % 5 == 0
-    "fizzbuzz"
+    "FizzBuzz"
   elsif int % 3 != 0 || % 5 !=0
     "nil"
   end
