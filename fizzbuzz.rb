7a9b98cbdc3adfa18e0cfbe252529474e7f33c95
nancy sin <nsin@sbcglobal.net>
2018-02-20 20:42:35 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,8 +6,8 @@ def fizzbuzz(int)
     "Fizz" #
   elsif int % 5 == 0
     "buzz"
-  elsif int % 3 == 0 &&  % 5 == 0
+  elsif int % 3 == 0 && int % 5 == 0
     "fizzbuzz"
-  elsif int % 3 !== 0 || % 5 !==0
+  elsif int % 3 != 0 || % 5 !=0
     "nil"
   end
